45c9c98e809852bfc4b783c243e523b478377447
congthanhhh <congthanhh203@gmail.com>
2025-08-13 00:41:20 +0700

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import '../global.css';
+import { Text, View, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>Welcome to the Home Screen!</Text>
+        </View>
+    );
+}
