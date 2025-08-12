05681a5b27d36179415c6a5b6e1d7be28270c16a
congthanhhh <congthanhh203@gmail.com>
2025-08-13 01:30:52 +0700

    install gluestack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,12 @@
 import { Slot, Stack, Tabs } from "expo-router";
 
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 export default function RootLayout() {
-    return <Tabs />;
+    return (
+        <GluestackUIProvider mode="light">
+            <Stack />
+        </GluestackUIProvider>
+    );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,25 @@
 import '../global.css';
-import { Text, View, StyleSheet } from "react-native";
+import { View } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Card } from "@/components/ui/card"
+import { Heading } from "@/components/ui/heading"
+import { Text } from "@/components/ui/text"
+
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text>Welcome to the Home Screen!</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-lg mb-4">Welcome to the Home Screen!</Text>
+            <Button size="md" variant="solid" action="positive">
+                <ButtonText>Hello World!</ButtonText>
+            </Button>
+
+            <Card size="md" variant="elevated" className="m-3">
+                <Heading size="lg" className="mb-1">
+                    Quick Start
+                </Heading>
+                <Text size="sm">Start building your next project in minutes</Text>
+            </Card>
         </View>
     );
 }
